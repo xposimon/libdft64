77e25558ad4854c09740a97a00774279b5673a54
xposimon <472032034@qq.com>
2022-01-24 23:42:30 +0800

    direct cmp hook

--- tools/cmp_hook.cpp
+++ tools/cmp_hook.cpp
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-12-06 21:52:44
  * @LastEditors: zx Zhou
- * @LastEditTime: 2022-01-21 22:48:24
+ * @LastEditTime: 2022-01-24 23:40:56
  * @FilePath: /libdft64/tools/cmp_hook.cpp
  */
 #include "branch_pred.h"
@@ -11,6 +11,11 @@
 #include <iostream>
 
 #define max(a,b) (a>b?a:b)
+#ifdef x86 
+#define DEFAULT_MEM_READ_SIZE (4)
+#else
+#define DEFAULT_MEM_READ_SIZE (8)
+#endif
 
 typedef struct {
   size_t largest_cmp;
@@ -146,7 +151,7 @@ VOID CmpFuncTrace(IMG img, VOID *v)
 
 /*
 Ins Category:
-  14 sub/cmp
+  14 add/sub/cmp
   15 bt
   26 cmovnz
   31 mov/movups
@@ -161,9 +166,33 @@ Ins Category:
 
 int cnt =0;
 
-VOID memory_taint_check(ADDRINT mem_addr1, ADDRINT mem_addr2)
+// Multiple defs to reduce instrument size
+
+VOID memory_taint_check_mvi(ADDRINT mem_addr, size_t num, ADDRINT ret_addr)
 {
-    printf("%ld %ld\n", mem_addr1, mem_addr2);
+    printf("addr:%lx\n", mem_addr);
+    size_t tagl=0;
+    if (mem_addr)
+        tagl = tagmap_get_len((ADDRINT)mem_addr, num);
+    printf("[+] direct mvi cmp len: %ld, addr: %lx\n", tagl, ret_addr);
+
+}
+
+VOID memory_taint_check_mvm(ADDRINT mem_addr0, ADDRINT mem_addr1, size_t num0, size_t num1, ADDRINT ret_addr)
+{
+    printf("addr1:%lx, addr2:%lx\n", mem_addr0, mem_addr1);
+    size_t tagl0=0, tagl1=0;
+    if (mem_addr0)
+        tagl0 = tagmap_get_len((ADDRINT)mem_addr0, num0);
+    if (mem_addr1)
+        tagl1 = tagmap_get_len((ADDRINT)mem_addr1, num1);
+    printf("[+] direct mvm cmp len: %ld, addr: %lx\n", max(tagl0, tagl1), ret_addr);
+
+}
+
+VOID reg_taint_check(THREADID tid, unsigned int reg_idx, size_t n)
+{
+    return;
 }
 
 VOID DirectCmpHook(TRACE trace, VOID *v)
@@ -178,30 +207,85 @@ VOID DirectCmpHook(TRACE trace, VOID *v)
                 // sub && cmp
                 if (cat == 14){
                     //printf("[++] %d\n", cnt ++);
-                    
-                    //printf("[dis] %s : [cate] %d\n", INS_Disassemble(branch_set_ins).c_str(), cat);
- 
-                    if (INS_OperandIsMemory(branch_set_ins, 0) || INS_OperandIsMemory(branch_set_ins, 1)){
-                            INS_InsertCall(branch_set_ins, IPOINT_BEFORE, (AFUNPTR)memory_taint_check,
-                            IARG_MEMORYREAD_EA,
-                            IARG_MEMORYREAD2_EA,
-                            IARG_END);
-                            continue;
+                    // enumerate different situations
+                    printf("[dis] %s : [cate] %d\n", INS_Disassemble(branch_set_ins).c_str(), cat);
+
+                    // extract registers if there are 
+                    REG reg0=REG_INVALID_, reg1=REG_INVALID_;
+                    if (INS_OperandIsReg(branch_set_ins, 0)){
+                        reg0 = INS_OperandReg(branch_set_ins, 0);
+                        printf("reg[0] %s\n", REG_StringShort(reg0).c_str());   
                     }
 
-                    for (int reg_i = 0; reg_i < 2; reg_i ++){
-                        if (INS_OperandIsReg(branch_set_ins, reg_i)){
-                            //REG reg = INS_OperandReg(branch_set_ins, reg_i);
-                            //printf("reg[%d] %s\n", reg_i, REG_StringShort(reg).c_str());
+                    if (INS_OperandIsReg(branch_set_ins, 1)){
+                        reg1 = INS_OperandReg(branch_set_ins, 1);
+                        printf("reg[1] %s\n", REG_StringShort(reg1).c_str()); 
+                    }
+
+                    if (!INS_IsMemoryRead(branch_set_ins)){
+                        // check register taint
+                        continue;
+                    }
+                    
+                    if (INS_OperandIsMemory(branch_set_ins, 0)){
+                        if(INS_OperandIsMemory(branch_set_ins, 1)){
+                            // cmp [mem1], [mem2]
+                            size_t num0, num1;
+                            if (INS_HasScatteredMemoryAccess(branch_set_ins))
+                                num0 = INS_MemoryOperandSize(branch_set_ins, 0); 
+                            else num0 = DEFAULT_MEM_READ_SIZE; 
+                            
+                            if (INS_HasScatteredMemoryAccess(branch_set_ins))
+                                num1 = INS_MemoryOperandSize(branch_set_ins, 1);
+                            else num1 = DEFAULT_MEM_READ_SIZE; 
+                            
+                            INS_InsertCall(branch_set_ins, IPOINT_BEFORE, (AFUNPTR)memory_taint_check_mvm,
+                            IARG_MEMORYREAD_EA,
+                            IARG_MEMORYREAD2_EA,
+                            IARG_UINT32, num0,
+                            IARG_UINT32, num1, 
+                            IARG_INST_PTR,
+                            IARG_END);
+                            continue;
+                        }
+                        else{
+                            // cmp [mem0], imm
+                            size_t num0;
+                            if (INS_HasScatteredMemoryAccess(branch_set_ins))
+                                num0 = INS_MemoryOperandSize(branch_set_ins, 0); 
+                            else 
+                                num0 = DEFAULT_MEM_READ_SIZE; 
+                            
+                            INS_InsertCall(branch_set_ins, IPOINT_BEFORE, (AFUNPTR)memory_taint_check_mvi,
+                                IARG_MEMORYREAD_EA,
+                                IARG_UINT32, num0, 
+                                IARG_INST_PTR,
+                                IARG_END);
                             continue;
                         }
                     }
+                    else{
+                        if(INS_OperandIsMemory(branch_set_ins, 1)){
+                            // cmp imm, [mem1]
+                            size_t num1;
+                            if (INS_HasScatteredMemoryAccess(branch_set_ins))
+                                num1 = INS_MemoryOperandSize(branch_set_ins, 1); 
+                            else 
+                                num1 = DEFAULT_MEM_READ_SIZE; 
+                            INS_InsertCall(branch_set_ins, IPOINT_BEFORE, (AFUNPTR)memory_taint_check_mvi,
+                                IARG_MEMORYREAD_EA,
+                                IARG_UINT32, num1,
+                                IARG_INST_PTR,
+                                IARG_END);
+                            continue;
+                        }
+                    }
+                    
                 }
             }
         }
     }
 }
-
 
 int main(int argc, char *argv[]) {
 
